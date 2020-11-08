74714f5d9fd32c3b3c10aea89c5a1d55aa2a91ab
bybrisk <imctobybrisk@gmail.com>
2020-11-09 00:58:15 +0530

    added

--- docs/docs.go
+++ docs/docs.go
@@ -1,6 +1,6 @@
 // Package classification delivery API.
 //
-// Documentation for delivery API
+// Documentation for delivery API.
 // the purpose of this application is to provide an application
 // that is using plain go code to define an API
 //
