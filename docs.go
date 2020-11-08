9d1b8d57c3393a9d7b1c3920b3a723156e030ea4
bybrisk <imctobybrisk@gmail.com>
2020-11-08 23:38:34 +0530

    new swagger comment added

--- docs.go
+++ docs.go
@@ -1,20 +1,11 @@
 // Package classification of delivery API.
 //
-// This API adds, updates, modifies and deletes the delivery
-// for to the bybrisk maps platform
-//
-// Delivery API handles GET, POST, PUT and DELETE method
-// Documentation for delivery API
-//
-// Terms Of Service:
-//
-// there are no TOS at this moment and we are providing API for free, use as much as you want
+// Documentation for delivery API 
 //
 //     Schemes: http, https
 //     Host: api.bybrisk.com
 //     BasePath: /
 //     Version: 0.0.1
-//     License: MIT http://opensource.org/licenses/MIT
 //     Contact: Shashank P. Sharma <imctobybrisk@gmail.com> http://csol99.blogspot.com
 //
 //     Consumes:
@@ -27,14 +18,14 @@
 
 package docs
 
-//import "github.com/bybrisk/delivery-api/data"
+import "github.com/bybrisk/delivery-api/data"
 
-/*// swagger:route POST /delivery delivery-methods POST
-// delivery API uploads a new delivery to bybrisk system.
+// swagger:route POST /delivery delivery-methods POST
+// delivery API uploads a new delivery to bybrisk maps Platform.
 // responses:
 //   200: deliveryResponse
 
-// This text will appear as description of your response body.
+// This is the response body.
 // swagger:response deliveryResponse
 type foobarResponseWrapper struct {
 	// in:body
@@ -43,7 +34,7 @@ type foobarResponseWrapper struct {
 
 // swagger:parameters idOfdeliveryEndpoint
 type foobarParamsWrapper struct {
-	// This text will appear as description of your request body.
+	// This is the schema of your request body.
 	// in:body
 	Body data.DeliveryDetail
-}*/
+}
